adfeaf0c794b1ae427e9ce89edcc331732b95fe3
JessicaWiedemeier <jessica.m.wiedemeier-1@ou.edu>
2019-03-04 17:01:56 -0600

    create branch; read IMET from JSON

--- Raw_Profile.py
+++ Raw_Profile.py
@@ -7,12 +7,16 @@ Copyright University of Oklahoma Center for Autonomous Sensing and Sampling
 """
 
 import pint
+import json
+import pandas as pd
+import numpy as np
+from pandas.io.json import json_normalize
 
 units = pint.UnitRegistry()
 
 
 class Raw_Profile():
-    """ Contains data from one file.
+    """ Contains data from one file. Data is stored as a pandas DataFrame.
 
     :var tuple temp: temperature as (voltage1, voltage2, ..., time: ms)
     :var tuple rh: relative humidity as (rh1, rh2, ..., time: ms)
@@ -36,6 +40,7 @@ class Raw_Profile():
         self.pres = None
         self.rotation = None
         self.dev = None
+        self._read_JSON(file_path)
 
     def thermo_data(self):
         """ Gets data needed by the Thermo_Profile constructor.
@@ -58,11 +63,72 @@ class Raw_Profile():
         return: [rotation]
         """
 
-    def _read_JSON(self):
+    def _read_JSON(self, file_path):
         """ Reads data from a .JSON file. Called by the constructor.
         """
 
-    def read_netCDF(self):
+        # Read the file into a list which pandas can normalize and read
+        full_data = []
+        for line in open(file_path, 'r'):
+            full_data.append(json.loads(line))
+
+        """
+        Now full_data is a list of JSON element with 2 dictionaries each. If
+        we refer to one JSON element as "tweet", the structure can be described
+        as follows:
+
+        tweet["meta"] contains "timestamp" and "type".
+
+        tweet["data"] depends on tweet["meta"]["type"]. IMET, for example,
+        could contain
+            Temp1, float, 1, 286.2941589355469
+            Temp2, float, 1, 286.27020263671875
+            Temp3, float, 1, 286.0711364746094
+            Temp4, float, 1, 0.0
+            Time, int, 1, 60898080
+            Volt1, float, 1, 4441.3125
+            Volt2, float, 1, 4431.5625
+            Volt3, float, 1, 4429.875
+            Volt4, float, 1, 0.0
+
+        Next, we iterate through full_data and identify line containing the
+        types we want to keep. We then extract the data from each element
+        using a different code for each type.
+        """
+        temp_list = None
+        # sensor_names will be a dictionary formatted {name: index}
+        sensor_names = {}
+        for elem in full_data:
+
+            # IMET
+            if elem["meta"]["type"] == "IMET":
+
+                # First time only - setup temp_list
+                if temp_list is None:
+                    # Create array of lists with one list per temperature
+                    # sensor reported in the data file, plus one for times
+                    temp_list = [[] for x in range(sum('Volt' in s for s in
+                                 elem["data"].keys())+1)]
+
+                    # Determine field names
+                    sensor_numbers = np.add(range(len(temp_list)-1), 1)
+                    for num in sensor_numbers:
+                        sensor_names["Volt"+str(num)] = num - 1
+                    sensor_names["Time"] = -1
+
+                # Read fields into temp_list, including Time
+                for key, value in sensor_names.items():
+                    try:
+                        temp_list[value].append(elem["data"][key])
+                    except KeyError:
+                        temp_list[value].append(np.nan)
+
+            elif elem["meta"]["type"] == "potato":
+                print("something's wrong here")
+
+        self.temp = tuple(temp_list)
+
+    def _read_netCDF(self):
         """ Reads data from a NetCDF file. Called by the constructor.
         """
 
