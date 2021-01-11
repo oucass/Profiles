c2a7a9aaa3322e765520569f6b3c8ff2928443d7
TMBell05 <tyler.m.bell05@gmail.com>
2021-01-11 11:55:56 -0600

    Small change to buildDocs.sh

--- docsrc/buildDocs.sh
+++ docsrc/buildDocs.sh
@@ -20,6 +20,7 @@ apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-stemmer python3-git python3-pip python3-virtualenv python3-setuptools
 
 python3 -m pip install --upgrade rinohtype pygments
+python3 -m pip install .
 
 #####################
 # DECLARE VARIABLES #
@@ -34,10 +35,6 @@ docroot=`mktemp -d`
 
 export REPO_NAME="${GITHUB_REPOSITORY##*/}"
 
-#####################
-# build oucass-profiles #
-#####################
-pip install -e .
 
 ##############
 # BUILD DOCS #
